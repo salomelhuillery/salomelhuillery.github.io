da597cc690aa8f0865b8a77afc8f57e4afcb4771
salomelhuillery <96081781+salomelhuillery@users.noreply.github.com>
2022-01-31 13:57:16 +0100

    Added main files

---
+++ jssalome.js
@@ -0,0 +1,54 @@
+function opencv()
+{document.getElementById('cv').style.display = "block";}
+
+
+function openarticle()
+{document.getElementById('article').style.display = "block";}
+
+function openfrench()
+{document.getElementById('french').style.display = "block";}
+
+function openeng()
+{document.getElementById('eng').style.display = "block";}
+
+function closecv()
+{cv.style.display = 'none';}
+
+function closearticle()
+{article.style.display = 'none';}
+
+function closefrench()
+{french.style.display = 'none';}
+
+function closeeng()
+{eng.style.display = 'none';}
+
+function closemusic()
+{music.style.display = 'none';}
+
+
+
+function closeavatar()
+{avatar.style.display = 'none';}
+
+function closeint()
+{int.style.display = 'none';}
+
+
+//  function maximize(){
+//     if (document.getElementById('cv').style.display = "block")
+//     {
+//     document.getElementById("cv").style.width=100%;}
+//  }
+
+
+
+
+
+
+function sendemail() 
+{
+window.location = "mailto:salomelhuillery@gmail.com";
+}
+
+
